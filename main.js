0f8b3c793c7e0b8643ae3eb0d6a5661b1aa67030
Leon <leonivanrodrigo@gmail.com>
2019-03-27 20:13:12 -0300

    Finalizado primeros 5 ejercicios.

---
+++ main.js
@@ -0,0 +1,27 @@
+// Ejercicio 1
+var myName = "Leon";
+console.log(myName + ".");
+
+// Ejercicio 2
+var leonAge = 33;
+console.log("Edad: " + leonAge + ".");
+
+// Ejercicio 3
+var ignasiAge = 32;
+var ageDiff = leonAge - ignasiAge;
+console.log("Age difference: " + ageDiff + ".");
+
+// Ejercicio 4
+if (leonAge > 21) {
+  console.log("You are older than 21.");
+} else {
+  console.log("You are not older than 21.");
+}
+// Ejercicio 5
+if (leonAge === ignasiAge) {
+  console.log("You have the same age as Ignasi");
+} else if (leonAge > ignasiAge) {
+  console.log("You are older than Ignasi.");
+} else {
+  console.log("Ignasi is older than you.");
+}
