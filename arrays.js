09c9a1921a76f628a549169b1f88b8d9e508c116
Leon <leonivanrodrigo@gmail.com>
2019-03-28 01:08:51 -0300

    Practicas con Arrays.

---
+++ arrays.js
@@ -0,0 +1,54 @@
+////////////////
+// Ejercicio 1
+// var companieros = ["Eliana", "Leon", "Adriana", "Aibby", "Alan", "Ayelen", "Birdie", "Carla", "Cintia", "David", "Diego", "Edgar", "Federico", "Fernando", "Andrés", "Giannina", "Luis", "Ignacio", "Janneth", "Juan", "Laura", "Lauri", "Marcelo", "Margara", "Maria", "Mariana", "Matias", "Nazarena", "Rodrigo", "Scarlet", "Stefania", "Stephanie", "Victoria C.", "Victoria", "Yanina", "Judith"];
+// // Ordenar elementos alfabeticamente
+// companieros.sort();
+// // Imprimir primer item del array
+// console.log("Primer ítem del array: ");
+// console.log(companieros[0]);
+// // Imprimir segundo item del array
+// console.log("Último ítem del array: ");
+// console.log(companieros[(companieros.length) - 1]);
+// // Mostrar todos los elementos del array
+// console.log("Elementos ordenados: ");
+// for (var i = 0; i < companieros.length; i++) {
+//   console.log(companieros[i]);
+// }
+////////////////
+//Ejercicio 2
+// var edadesCompas = [24, 35, 23, 26, 20, 18, 27, 26, 36, 33, 32, 34, 36, 37, 29, 27, 25, 22, 23, 26, 24, 36, 33, 32, 34, 36, 37, 29, 27, 25, 22, 23, 26, 24, 19, 25, 33];
+// var i = edadesCompas.length;
+// //Mostras edades con while
+// console.log("Edades mostradas con bucle while: ");
+// while (i > 0) {
+//   console.log(edadesCompas[i - 1]);
+//   i--;
+// }
+// console.log("Fin array.");
+// // Mostrar solo edades pares:
+// var i = edadesCompas.length;
+// console.log("Edades pares con bucle while: ");
+// while (i > 0) {
+//   if ((edadesCompas[i - 1] % 2) === 0) {
+//     console.log(edadesCompas[i - 1]);
+//   }
+//   i--;
+// }
+// console.log("Fin array.");
+// //Mostar edades con for:
+// console.log("Edades mostradas con bucle for: ");
+// for (var i = 0; i < edadesCompas.length; i++) {
+//   console.log(edadesCompas[i]);
+// }
+// console.log("Fin array.");
+// // Edades pares con bucle for
+// console.log("Edades pares mostradas con bucle for: ");
+// for (var i = 0; i < edadesCompas.length; i++) {
+//   if (edadesCompas[i] % 2 == 0) {
+//     console.log(edadesCompas[i]);
+//   }
+//
+// }
+// console.log("Fin array.");
+///////////////
+// Ejercicio 3
